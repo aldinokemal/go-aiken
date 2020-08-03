eab1661ed2b39398e4e979538a6ac186fa428658
aldinokemal <aldinokemal2104@gmail.com>
2020-08-03 17:32:04 +0700

    [master]

--- Aiken_test.go
+++ Aiken_test.go
@@ -1,12 +1,11 @@
 package Aiken
 
 import (
-	aiken "github.com/aldinokemal/go-aiken"
 	"testing"
 )
 
 func TestReadAiken(t *testing.T) {
-	result, err := aiken.ReadAiken("aiken-example.txt")
+	result, err := ReadAiken("aiken-example.txt")
 	if err != nil {
 		t.Error(err.Error())
 	} else {
