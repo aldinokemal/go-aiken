9c65f47648fdb208bf7fec8a2573b343fa5b2616
aldinokemal <aldinokemal2104@gmail.com>
2020-08-03 17:17:46 +0700

    [master]

--- example/main.go
+++ example/main.go
@@ -2,55 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"regexp"
-	"strings"
+	aiken "github.com/aldinokemal/go-aiken"
 )
 
-type Aiken struct {
-	Question string            `json:"question"`
-	Options  map[string]string `json:"options"`
-	Answer   string            `json:"answer"`
-}
-
-func handleAiken(path string) (result []Aiken, err error) {
-	names, err := ioutil.ReadFile(path)
-
-	//question := make(map[string]string)
-	if err == nil {
-		start := true
-		content := string(names)
-		contentPerLines := strings.Split(content, "\n")
-
-
-		var tmpData Aiken
-		tmpOptions := make(map[string]string)
-		for _, val := range contentPerLines {
-			line := strings.Trim(val, " ")
-			isAnswer, _ := regexp.MatchString("^ANSWER:{1}", line)
-			if line != "" {
-				if start {
-					tmpData.Question = strings.TrimSuffix(line, "\r")
-					start = false
-				} else if isAnswer {
-					answer := regexp.MustCompile(`^ANSWER:{1}`).ReplaceAllString(line, "")
-					tmpData.Answer = strings.TrimSuffix(answer, "\r")
-					tmpData.Options = tmpOptions
-					result = append(result, tmpData)
-					start = true
-					tmpData = Aiken{}
-					tmpOptions = map[string]string{}
-				} else {
-					options := strings.SplitN(line, " ", 2)
-					tmpOptions[regexp.MustCompile(`\.|\)`).ReplaceAllString(options[0], "")] = strings.Trim(options[1], " \r")
-				}
-			}
-		}
-	}
-	return
-}
-
 func main() {
-	result, _ := handleAiken("aiken-example.txt")
+	result, _ := aiken.ReadAiken("aiken-example.txt")
 	fmt.Println(result)
 }
